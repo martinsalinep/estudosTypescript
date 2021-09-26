63896da32e4a10543432eaa7230057364f1c777f
martinsalinep <alinemartins@hotmail.com.br>
2021-09-26 16:11:44 -0300

    webpack

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/Aula0018-webpack/index.ts":
+/*!***************************************!*\
+  !*** ./src/Aula0018-webpack/index.ts ***!
+  \***************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: TypeScript emitted no output for D:\\GitHub\\Pessoal\\cursoTS\\src\\Aula0018-webpack\\index.ts.\n    at makeSourceMapAndFinish (D:\\GitHub\\Pessoal\\cursoTS\\node_modules\\ts-loader\\dist\\index.js:52:18)\n    at successLoader (D:\\GitHub\\Pessoal\\cursoTS\\node_modules\\ts-loader\\dist\\index.js:39:5)\n    at Object.loader (D:\\GitHub\\Pessoal\\cursoTS\\node_modules\\ts-loader\\dist\\index.js:22:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/Aula0018-webpack/index.ts"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
